0619a42ba64245f723f3598a10474fe971ff72bf
Tatiana <poluektova.ta@gmail.com>
2020-06-29 21:09:13 +0300

    homework 11 added

---
+++ lesson 11/ex1-optimization.sql
@@ -0,0 +1,43 @@
+-- Задание 1. По теме “Оптимизация запросов”
+use shop;
+
+DROP TABLE IF EXISTS logs;
+CREATE TABLE logs (
+	created_at DATETIME NOT NULL,
+	table_name VARCHAR(45) NOT NULL,
+	str_id BIGINT(20) NOT NULL,
+	name_value VARCHAR(255) NOT NULL
+) ENGINE = ARCHIVE;
+
+
+DROP TRIGGER IF EXISTS log_users;
+delimiter //
+CREATE TRIGGER log_users AFTER INSERT ON users
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (created_at, table_name, str_id, name_value)
+	VALUES (NOW(), 'users', NEW.id, NEW.name);
+END //
+delimiter ;
+
+DROP TRIGGER IF EXISTS log_catalogs;
+delimiter //
+CREATE TRIGGER log_catalogs AFTER INSERT ON catalogs
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (created_at, table_name, str_id, name_value)
+	VALUES (NOW(), 'catalogs', NEW.id, NEW.name);
+END //
+delimiter ;
+
+
+DROP TRIGGER IF EXISTS log_products;
+delimiter //
+CREATE TRIGGER log_products AFTER INSERT ON products
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (created_at, table_name, str_id, name_value)
+	VALUES (NOW(), 'products', NEW.id, NEW.name);
+END //
+delimiter ;
+
